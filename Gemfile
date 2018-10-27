88a9f28dae4161fc1ffbe06b34838aa62c5fe106
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-10-27 15:40:03 +1300

    update some styles

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'simple_form'
 gem 'redis', '~> 4.0'
+gem 'jquery-rails'
 gem 'bootstrap', '~> 4.1.3'
 gem 'draper'
 
--- app/decorators/invite_decorator.rb
+++ app/decorators/invite_decorator.rb
@@ -1,6 +1,7 @@
 class InviteDecorator < ApplicationDecorator
   def name
-    return "#{object.people.order(primary: :desc).pluck(:last_name).uniq.to_sentence} Family" if object.people.many?
+    return "#{object.people.order(primary: :desc).pluck(:last_name).uniq.to_sentence} Whanau" if object.people.many?
+
     "#{object.primary_person.first_name} #{object.primary_person.last_name}".strip
   end
 end
