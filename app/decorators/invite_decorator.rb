93b192235c8636983cd1dce51bda4c1573feb392
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-11-27 20:12:55 +1300

    fix decorator

--- app/decorators/invite_decorator.rb
+++ app/decorators/invite_decorator.rb
@@ -4,10 +4,14 @@ class InviteDecorator < ApplicationDecorator
   def name
     return "#{names_to_sentence} Whanau" if object.people.many?
 
-    "#{object.primary_person.first_name} #{object.primary_person.last_name}".strip
+    "#{primary_person.first_name} #{primary_person.last_name}".strip
   end
 
   protected
+
+  def primary_person
+    object.primary_person || object.people.first
+  end
 
   def names_to_sentence
     object.people.order(primary: :desc).pluck(:last_name).uniq.to_sentence
