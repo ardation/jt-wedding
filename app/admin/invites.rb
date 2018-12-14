65a93d3de8ec535fb7553ec9f05c99121117305a
Tataihono Nikora <tataihono.nikora@gmail.com>
2018-12-14 12:46:02 +0800

    update invite view

--- app/admin/invites.rb
+++ app/admin/invites.rb
@@ -1,26 +1,61 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Invite do
-  permit_params :reception, :ask_food, :rsvp, :food_type,
-                people_attributes: %i[primary first_name last_name gender child coming _destroy id]
+  permit_params :reception, :ask_food, :rsvp, :food_type, :phone,
+                people_attributes: %i[primary first_name last_name gender age coming _destroy id]
   decorate_with InviteDecorator
 
   index do
     selectable_column
     id_column
-    column :name
+    column :admin_name
     column :code
     column :rsvp
     column :created_at
     actions
   end
 
+  show do
+    columns do
+      column span: 6 do
+        panel 'People' do
+          table_for invite.people do
+            column :first_name
+            column :last_name
+            column :age
+            column :coming
+            column :primary
+          end
+        end
+        active_admin_comments
+      end
+      column span: 6 do
+        panel 'Details' do
+          attributes_table_for invite do
+            row :phone
+            row :style
+            row :food_type
+            row :rsvp
+            row :email
+            row :street
+            row :suburb
+            row :city
+            row :postal_code
+            row :country
+          end
+        end
+      end
+    end
+  end
+
   form do |f|
+    f.semantic_errors *f.object.errors.keys
     f.inputs 'Details' do
       f.input :reception
       f.input :ask_food
       f.input :rsvp
       f.input :food_type, collection: Invite.food_types, as: :radio
+      f.input :phone
     end
     f.inputs 'People' do
       f.has_many :people, allow_destroy: true, new_record: true, heading: nil do |p|
@@ -28,7 +63,7 @@ ActiveAdmin.register Invite do
         p.input :first_name
         p.input :last_name
         p.input :gender, collection: Invite::Person.genders, as: :radio
-        p.input :child
+        p.input :age, collection: Invite::Person.ages, as: :radio
         p.input :coming
       end
     end
--- app/decorators/invite_decorator.rb
+++ app/decorators/invite_decorator.rb
@@ -7,6 +7,10 @@ class InviteDecorator < ApplicationDecorator
     "#{primary_person.first_name} #{primary_person.last_name}".strip
   end
 
+  def admin_name
+    object.people.order(primary: :desc).pluck(:first_name).to_sentence
+  end
+
   protected
 
   def names_to_sentence
