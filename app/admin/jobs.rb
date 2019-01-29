3e55ec4a9255e4556c960e9ec0145905e5d1b95a
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-01-30 08:00:27 +1300

    update styles and job admin

--- app/admin/jobs.rb
+++ app/admin/jobs.rb
@@ -25,4 +25,39 @@ ActiveAdmin.register Job do
     end
     f.actions
   end
+
+  show do
+    columns do
+      column span: 6 do
+        panel 'Job Details' do
+          attributes_table_for job do
+            row :title
+            row(:url) do |job|
+              link_to job.url, job.url, target: '_blank'
+            end
+            row :created_at
+            row :updated_at
+          end
+        end
+        active_admin_comments
+      end
+      column span: 6 do
+        panel 'People' do
+          table_for job.people do
+            column :first_name
+            column :last_name
+            column(:coming) do |person|
+              status_tag(person.invite.rsvp? && person.coming?)
+            end
+            column(:coming_reception) do |person|
+              status_tag(person.invite.rsvp? && person.coming_reception?)
+            end
+            column(:actions) do |person|
+              link_to 'Show Invite', admin_invite_path(person.invite.id)
+            end
+          end
+        end
+      end
+    end
+  end
 end
