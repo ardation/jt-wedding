8ebe3c70383db5a02ed118ab04d56aa5d7873a33
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-01-29 16:19:16 +1300

    add job descriptions to home page

--- app/models/invite/person.rb
+++ app/models/invite/person.rb
@@ -6,6 +6,6 @@ class Invite::Person < ApplicationRecord
   validates :first_name, :last_name, :gender, :age, presence: true
   enum gender: { male: 'male', female: 'female' }
   enum age: { adult: 'adult', child: 'child' }
-  default_scope { order(primary: :desc, age: :desc) }
+  default_scope { order(primary: :desc, age: :asc) }
   delegate :title, :url, to: :job, prefix: true, allow_nil: true
 end
